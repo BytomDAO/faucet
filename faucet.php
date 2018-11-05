a8057f29a2beab0b8509fb50aaaf65d9a3296f63
Chengcheng Zhang <943420582@qq.com>
2018-11-05 20:19:45 +0800

    update

--- faucet.php
+++ faucet.php
@@ -99,6 +99,6 @@ if(strcmp($obj_resp['status'],'success')!==0){
     exit(-1);
 }
 
-echo '发送比原交易成功，交易ID：<a href="http://52.82.46.157:8082/tx/',$obj_resp['data']['tx_id'],'" target="_blank">',$obj_resp['data']['tx_id'],'</a>',"<br><br>\n";
+echo '发送比原交易成功，交易ID: <a href="http://52.82.46.157:8082/tx/',$obj_resp['data']['tx_id'],'" target="_blank">',$obj_resp['data']['tx_id'],'</a>',"<br><br>\n";
 echo '请等待2-3分钟得到比原链出块确认，然后打开你的比原钱包即可看到（注意钱包需接入比原测试网络testnet）。';
-echo '<p>返回<a href="http://test.blockmeta.com/faucet.php"></a></p>';
+echo '<p><a href="http://test.blockmeta.com/faucet.php">返回</a></p>';
