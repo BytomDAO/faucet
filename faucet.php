f7f3368c8438e9b1f5ad78bbe71fe313ff84e887
Chengcheng Zhang <943420582@qq.com>
2018-11-09 11:41:40 +0800

    update

--- faucet.php
+++ faucet.php
@@ -41,7 +41,10 @@ if( !(strcasecmp($address_flag,'t')==0 || strcasecmp($address_flag,'s')==0) ){
 <?php
   exit(0);
 }else{
+  ?>
   echo "<script>alert(\"地址格式错误！Address format error！\"),location.href=\"faucet.php\";</script>";
+  <?php
+  exit(0);
 }
 
 $asset_id=addslashes(@$_REQUEST['asset_id']); //避免异常输入字符的安全性问题
