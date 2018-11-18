3588bc93242e40082fafb06ebe3c1381b4809482
Chengcheng Zhang <943420582@qq.com>
2018-11-18 16:27:27 +0800

    fix commet

--- faucet_gm.php
+++ faucet_gm.php
@@ -33,7 +33,7 @@ if( !(strcasecmp($address_flag,'g')==0 || strcasecmp($address_flag,'s')==0) ){
       <p>Please input Bytom testnet address which is start from gm...</p>
       <form name="form_faucet" id="form_faucet" action="faucet_gm.php" method="get">
         <p>您的比原测试钱包地址(Wallet Address)：<input size="50" name="your_address" id="your_address" autofocus="true" type="text"></p>
-        <button type="submit" id="game_send_trans_btn" class="btn">免费领取（Get now for free!）</button>
+        <button type="submit" id="game_send_trans_btn" class="btn">免费领取10BTM（Get now for free!）</button>
       </form>
       <p>比原官方钱包下载和安装请参考：<a target="_blank" href="http://8btc.com/thread-181537-1-1.html">使用说明</a> （注意运行钱包时选择测试网络才能参与领取测试币）</p>
     </div>
